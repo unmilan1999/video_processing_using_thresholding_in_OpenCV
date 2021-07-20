ab45d9d1d396b009e2a335140b3a98f05785a235
unmilan1999 <86911131+unmilan1999@users.noreply.github.com>
2021-07-20 10:44:53 +0530

    Update pic.py

--- pic.py
+++ pic.py
@@ -13,8 +13,18 @@ def change_type(var):
     q = int(var)
     
 def video_out(image, x, y):
+    if x == 1:
+        _, thresh = cv2.threshold(blur, y, 255, cv2.THRESH_BINARY)
+    if x == 2:
+        _, thresh = cv2.threshold(blur, y, 255, cv2.THRESH_BINARY_INV)
+    if x == 3:
+        _, thresh = cv2.threshold(blur, y, 255, cv2.THRESH_TRUNC)
+    if x == 4:
+        _, thresh = cv2.threshold(blur, y, 255, cv2.THRESH_TOZERO)
+    if x == 5:
+        _, thresh = cv2.threshold(blur, y, 255, cv2.THRESH_TOZERO_INV)
+    cv2.imshow('My Cam', thresh)
  
-
 video = cv2.VideoCapture(0)
 while True:
     if cv2.waitKey(1) & 0xFF == ord('t'):
@@ -32,20 +42,8 @@ while True:
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(img, (5, 5), 0)
     video_out(blur, q, p)
-       if x == 1:
-        _, thresh = cv2.threshold(blur, y, 255, cv2.THRESH_BINARY)
-    if x == 2:
-        _, thresh = cv2.threshold(blur, y, 255, cv2.THRESH_BINARY_INV)
-    if x == 3:
-        _, thresh = cv2.threshold(blur, y, 255, cv2.THRESH_TRUNC)
-    if x == 4:
-        _, thresh = cv2.threshold(blur, y, 255, cv2.THRESH_TOZERO)
-    if x == 5:
-        _, thresh = cv2.threshold(blur, y, 255, cv2.THRESH_TOZERO_INV)
-    cv2.imshow('My Cam', thresh)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 video.release()
 cv2.destroyAllWindows()
-    
